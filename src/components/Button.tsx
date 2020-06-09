044cd06f5b8e256b5fecac8d2e93f1587a3d65e5
Zachary Thielemann <zachdt@gmail.com>
2020-06-09 12:29:54 -0400

    created .ts storybook template for material-ui

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+
+import styled from 'styled-components'
+
+import MuiButton from '@material-ui/core/Button'
+
+export const Button = styled(MuiButton)`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+`
